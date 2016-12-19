b0da4227ef2fa722176b54e6119ab2be1760c7c9
stephan.com <stephan@stephan.com>
2016-12-19 01:45:28 -0500

    clean up public controllers

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,4 @@
 class LocationsController < InheritedResources::Base
 	belongs_to :brand
-
-  private
-
-    def location_params
-      params.require(:location).permit(:name, :brand_id)
-    end
 end
 
--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,10 +1,4 @@
 class MenuItemsController < InheritedResources::Base
 	belongs_to :brand
-
-  private
-
-    def menu_item_params
-      params.require(:menu_item).permit(:name, :brand_id)
-    end
 end
 
